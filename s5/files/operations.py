58dd699997dec8ec8e3708fb50f28a0f48f4bb39
danielch1 <d.chroscicki@gmail.com>
2023-10-04 14:00:13 +0200

    [Add] Files to check

---
+++ s5/files/operations.py
@@ -0,0 +1,12 @@
+from utils import *
+
+def calculate():
+    a_add_b = add(1, 5)
+    a_sub_b = sub(1, 5)
+    a_divide_b = divide(1, 5)
+    a_multiply_b = multiply(2, 5)
+
+    return a_add_b, a_sub_b, a_divide_b, a_multiply_b
+
+if __name__ == "__main__":
+    print(calculate())
