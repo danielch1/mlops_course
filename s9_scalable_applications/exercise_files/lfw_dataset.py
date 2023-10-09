a096f8d6c66ace486f22a494e6b099dc37d0e56c
danielch1 <d.chroscicki@gmail.com>
2023-10-09 23:26:43 +0200

    [Add] S9 exercise

--- s9_scalable_applications/exercise_files/lfw_dataset.py
+++ s9_scalable_applications/exercise_files/lfw_dataset.py
@@ -12,22 +12,28 @@ from torchvision import transforms
 
 class LFWDataset(Dataset):
     def __init__(self, path_to_folder: str, transform) -> None:
-        # TODO: fill out with what you need
+        self.path_to_folder = path_to_folder
         self.transform = transform
 
     def __len__(self):
-        return None  # TODO: fill out
+        # You can implement this if needed
+        pass
 
     def __getitem__(self, index: int) -> torch.Tensor:
-        # TODO: fill out
+        # Load the image from file using PIL or any other method you prefer
+        img = load_image_from_file(self.path_to_folder, index)
         return self.transform(img)
 
+def load_image_from_file(path_to_folder, index):
+    # Implement a function to load the image from file (e.g., using PIL)
+    # Replace this with your own logic to load images
+    pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-path_to_folder", default="", type=str)
+    parser.add_argument("-path_to_folder", default="s9_scalable_applications/lfw", type=str)
     parser.add_argument("-batch_size", default=512, type=int)
-    parser.add_argument("-num_workers", default=None, type=int)
+    parser.add_argument("-num_workers", default=4, type=int)
     parser.add_argument("-visualize_batch", action="store_true")
     parser.add_argument("-get_timing", action="store_true")
     parser.add_argument("-batches_to_check", default=100, type=int)
@@ -43,8 +49,15 @@ if __name__ == "__main__":
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
 
     if args.visualize_batch:
-        # TODO: visualize a batch of images
-        pass
+        dataiter = iter(dataloader)
+        images, labels = next(dataiter)
+
+        def show_pil_images(images):
+            for image in images:
+                image = transforms.ToPILImage()(image)
+                image.show()
+
+        show_pil_images(images)
 
     if args.get_timing:
         # lets do some repetitions
@@ -59,4 +72,4 @@ if __name__ == "__main__":
             res.append(end - start)
 
         res = np.array(res)
-        print("Timing: {np.mean(res)}+-{np.std(res)}")
+        print(f"Timing: {np.mean(res)}+-{np.std(res)}")
